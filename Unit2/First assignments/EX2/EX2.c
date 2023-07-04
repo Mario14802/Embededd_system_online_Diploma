1e58e3fb7ff009c756053183e80c840b638c9c15
Mario14802 <mario.atef720@gmail.com>
2023-07-04 16:42:18 +0300

    'Unit2'

---
+++ Unit2/First assignments/EX2/EX2.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : EX2.c
+ Author      : Mario
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+int num1;
+
+printf("Enter a integar: \r ");
+fflush(stdin); fflush(stdout);
+scanf("%d",&num1);
+printf("you entered :%d",num1);
+
+
+
+	return (0);
+}
