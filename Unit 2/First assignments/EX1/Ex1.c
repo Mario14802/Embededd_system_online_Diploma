6a8b72bdbfe85b8b4437f638e4a76b07f12ce3c8
Mario14802 <mario.atef720@gmail.com>
2023-07-04 16:40:17 +0300

    'Unit2'

---
+++ Unit 2/First assignments/EX1/Ex1.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Ex1.c
+ Author      : Mario
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	printf("C Programming");
+	return (0);
+}
